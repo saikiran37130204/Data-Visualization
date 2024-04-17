4cbcafd10b8e5a7609ca7c348ca28b04ddb5dce8
saikiran37130204 <saikirankondreddy.1@gmail.com>
2024-04-17 12:50:01 -0500

    removed and modified some files

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
-var express = require('express');
-var router = express.Router();
+// routes/index.js
 
-/* GET home page. */
+const express = require('express');
+const router = express.Router();
+const path = require('path');
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.sendFile(path.join(__dirname, '..', 'views', 'index.html'));
 });
 
 module.exports = router;
